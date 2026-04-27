5585aa3e4c91e97984438c6e996e7b4d1ddd9015
Leonardo <leonardojulianhq@gmail.com>
2026-04-26 22:35:04 -0500

    Initial commit

---
+++ src/main/java/org/project/expressart/ExpressartApplication.java
@@ -0,0 +1,13 @@
+package org.project.expressart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpressartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/expressart/ExpressartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.expressart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
