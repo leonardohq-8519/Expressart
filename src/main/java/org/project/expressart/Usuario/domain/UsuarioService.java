9e83a3aed970b3b1b84f36e3e8516cffdc240353
Emily <emily.chacon@utec.edu.pe>
2026-05-19 18:18:49 -0500

    Modificaciones en UsuarioService

--- src/main/java/org/project/expressart/Usuario/domain/UsuarioService.java
+++ src/main/java/org/project/expressart/Usuario/domain/UsuarioService.java
@@ -1,1 +1,49 @@
 package org.project.expressart.Usuario.domain;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService{
+    @Autowired
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepo){
+        this.usuarioRepository = usuarioRepo;
+    }
+
+    public Page<Usuario> getAllProducts(Pageable pageable) {
+        return usuarioRepository.findAll(pageable);
+    }
+
+    public void agregarCliente(Usuario usuario){
+        usuarioRepository.save(usuario);
+    }
+
+    // TODO: seguir editando
+    public UsuarioResponseDTO getUsuario(Long id){
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    public void deleteUsuario(Long id) {usuarioRepository.deleteById(id);
+    }
+
+    public Usuario updateUsuario(Long id, Usuario usuario) {
+        Usuario usuarioActualizado = usuarioRepository.findById(id).orElse(null);
+        if (usuarioActualizado == null) {
+            return null;
+        }
+        usuarioActualizado.setNombre_usuario(usuario.getNombre_usuario());
+        usuarioActualizado.setEmail(usuario.getEmail());
+        usuarioActualizado.setNombre(usuario.getNombre());
+        usuarioActualizado.setContraseña(usuario.getContraseña());
+        usuarioActualizado.setAvatar_url(usuario.getAvatar_url());
+        usuarioActualizado.setBiografia(usuario.getBiografia());
+        usuarioActualizado.setToken(usuario.getToken());
+        return usuarioRepository.save(usuarioActualizado);
+    }
+
+}
