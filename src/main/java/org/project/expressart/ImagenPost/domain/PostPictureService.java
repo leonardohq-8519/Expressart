e60c852917b388769d80ef1544412f7cdcc922f1
Leonardo <leonardojulianhq@gmail.com>
2026-05-24 23:16:44 -0500

    Se modifican los services de ImagenPost e ImagenComision para usar Multipartfile en la subida de archivos.

--- src/main/java/org/project/expressart/ImagenPost/domain/PostPictureService.java
+++ src/main/java/org/project/expressart/ImagenPost/domain/PostPictureService.java
@@ -1,26 +1,28 @@
 package org.project.expressart.ImagenPost.domain;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.project.expressart.ArchivoPost.dto.ArchivoPostResponseDTO;
+import org.project.expressart.ArchivoPost.domain.FileService;
 import org.project.expressart.ImagenPost.dto.ImagenPostCreateDTO;
 import org.project.expressart.ImagenPost.dto.ImagenPostResponseDTO;
 import org.project.expressart.ImagenPost.dto.ImagenPostUpdateDTO;
 import org.project.expressart.ImagenPost.infrastructure.ImagenPostRepository;
-import org.project.expressart.Mensaje.domain.Mensaje;
-import org.project.expressart.Mensaje.dto.MessageResponseDTO;
 import org.project.expressart.Post.domain.Post;
 import org.project.expressart.Post.infrastructure.PostRepository;
+import org.project.expressart.exceptions.ImageLimitException;
 import org.project.expressart.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.awt.*;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class PostPictureService {
     @Autowired
     private ModelMapper modelMapper;
@@ -29,48 +31,106 @@ public class PostPictureService {
     @Autowired
     private final PostRepository postRepository;
 
+    private static final int IMAGE_LIMIT = 4;
+    @Autowired
+    private FileService fileService;
+
     public List<ImagenPostResponseDTO> getByPostId(Long postId) throws ResourceNotFoundException {
-        List<ImagenPost> postPicture = postPictureRepository.findByPostId(postId);
-        if (postPicture.isEmpty()) {
-            throw new ResourceNotFoundException("No post pictures found for post id: " + postId);
-        }
-        return postPicture.stream()
-                .map(ticket -> modelMapper.map(postPicture, ImagenPostResponseDTO.class))
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("No post pictures found for post id: " + postId));
+
+        return post.getImagenes().stream()
+                .map(image -> modelMapper.map(image, ImagenPostResponseDTO.class))
+                .sorted(Comparator.comparingInt(ImagenPostResponseDTO::getOrden))
                 .collect(Collectors.toList());
     }
 
-    public ImagenPostResponseDTO getById(Long id) throws ResourceNotFoundException {
-        ImagenPost postPicture = postPictureRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Post picture not found"));
-        return modelMapper.map(postPicture, ImagenPostResponseDTO.class);
+    public ImagenPostResponseDTO getById(Long postId, Long imageId) throws ResourceNotFoundException {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Post not found: " + postId
+                ));
+
+        return post.getImagenes().stream()
+                .filter(img -> img.getId().equals(imageId))
+                .findFirst()
+                .map(img -> modelMapper.map(img, ImagenPostResponseDTO.class))
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Image not found: " + imageId
+                ));
     }
-    public ImagenPostResponseDTO create(ImagenPostCreateDTO dto){
+
+    @Transactional
+    public ImagenPostResponseDTO create(Long postId, MultipartFile file){
+        Post post = postRepository.findById(postId).orElseThrow(()-> new EntityNotFoundException("Post picture not found"));
+
+        if (post.getImagenes().size() >= IMAGE_LIMIT) {
+            throw new ImageLimitException(
+                    "A post cannot have more than " + IMAGE_LIMIT + " images"
+            );
+        }
+
+        String url = fileService.uploadImagePost(file, postId);
+
         ImagenPost postPicture = new ImagenPost();
-        Post post = postRepository.findById(dto.getPostId()).orElseThrow(()-> new EntityNotFoundException("Post picture not found"));
+
         postPicture.setPost(post);
-        postPicture.setUrl(dto.getUrl());
-        postPicture.setOrden(dto.getOrden());
+        postPicture.setUrl(url);
+        postPicture.setOrden(post.getImagenes().size());
+
+        post.getImagenes().add(postPicture);
         postPictureRepository.save(postPicture);
+
         return modelMapper.map(postPicture, ImagenPostResponseDTO.class);
     }
 
     public ImagenPostResponseDTO update(Long id, ImagenPostUpdateDTO dto)throws ResourceNotFoundException{
-        ImagenPost postPicture = postPictureRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post picture not found"));;
+        ImagenPost postPicture = postPictureRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post picture not found"));
         postPicture.setUrl(dto.getUrl());
         postPicture.setOrden(dto.getOrden());
         postPictureRepository.save(postPicture);
         return modelMapper.map(postPicture, ImagenPostResponseDTO.class);
     }
 
-    public void delete(Long id){
-        if (postPictureRepository.existsById(id))
-            postPictureRepository.deleteById(id);
-        else
-            throw new EntityNotFoundException("Post picture with ID " + id + " doesn't exist");
+    public void delete(Long postId, Long imageId){
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new  EntityNotFoundException(
+                        "Post not found: " + postId
+                ));
+
+        ImagenPost image = post.getImagenes().stream()
+                .filter(img -> img.getId().equals(imageId))
+                .findFirst()
+                .orElseThrow(() -> new  EntityNotFoundException(
+                        "Image not found: " + imageId
+                ));
+
+        fileService.delete(image.getUrl());
+
+        post.getImagenes().remove(image);
+        reorder(post.getImagenes());
+        postRepository.save(post);
     }
     public void deleteByPost(Long postId){
-        if (postPictureRepository.existsByPostId(postId))
-            postPictureRepository.deleteByPostId(postId);
-        else
-            throw new EntityNotFoundException("Post picture from post ID " + postId + " doesn't exist");
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "Post not found: " + postId
+                ));
+
+        if (post.getImagenes().isEmpty()) {
+            throw new EntityNotFoundException(
+                    "Post: " + postId + " does not have images."
+            );
+        }
+        post.getImagenes().forEach(image -> fileService.delete(image.getUrl()));
+
+        post.getImagenes().clear();
+        postRepository.save(post);
+    }
+
+    private void reorder(List<ImagenPost> images){
+        for (int i = 0; i < images.size(); i++){
+            images.get(i).setOrden(i);
+        }
     }
 }
